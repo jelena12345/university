dc88c3f8c810a67e02b94beede3fe06d2aebabae
JMihailicenko <jelena.mihailicenko@gmail.com>
2021-02-11 13:54:13 +0200

    Completed IndexController tests

--- src/test/java/com/foxminded/controllers/IndexControllerTest.java
+++ src/test/java/com/foxminded/controllers/IndexControllerTest.java
@@ -1,19 +1,23 @@
 package com.foxminded.controllers;
 
-import com.foxminded.services.CourseService;
-import com.foxminded.services.UserCourseService;
+import com.foxminded.dto.UserDto;
 import com.foxminded.services.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import javax.servlet.http.HttpSession;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class IndexControllerTest {
@@ -29,16 +33,76 @@ class IndexControllerTest {
     }
 
     @Test
-    void testIndexPage() throws Exception {
+    void testIndexPage_ShouldReturnIndexPage() throws Exception {
         this.mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"));
     }
 
     @Test
-    void testRegistrationPage() throws Exception {
+    void testIndexPage_ShouldRedirectToProfilePage() throws Exception {
+        this.mockMvc.perform(get("/")
+                .sessionAttr("user", new UserDto()))
+                .andExpect(redirectedUrl("/profile"))
+                .andExpect(status().isFound());
+    }
+
+    @Test
+    void testRegistrationPage_ShouldReturnRegistrationPage() throws Exception {
         this.mockMvc.perform(get("/register"))
                 .andExpect(status().isOk())
+                .andExpect(model().attributeExists("user"))
                 .andExpect(view().name("user/registration"));
+    }
+
+    @Test
+    void testSignOut_ShouldRedirectToIndexPage() throws Exception {
+        HttpSession session = this.mockMvc.perform(get("/signOut")
+                .sessionAttr("user", new UserDto()))
+                .andExpect(redirectedUrl("/"))
+                .andExpect(status().isFound())
+                .andReturn()
+                .getRequest()
+                .getSession();
+        assertNotNull(session);
+        assertNull(session.getAttribute("user"));
+    }
+
+    @Test
+    void testSignIn_Successful_ShouldRedirectToProfilePage() throws Exception {
+        when(userService.existsByPersonalId(anyString())).thenReturn(true);
+        when(userService.findByPersonalId(anyString())).thenReturn(new UserDto());
+        HttpSession session = this.mockMvc.perform(post("/signIn"))
+                .andExpect(redirectedUrl("/profile"))
+                .andExpect(status().isFound())
+                .andReturn()
+                .getRequest()
+                .getSession();
+        assertNotNull(session);
+        assertNotNull(session.getAttribute("user"));
+    }
+
+    @Test
+    void testSignIn_Unsuccessful_ShouldRedirectToIndexPage() throws Exception {
+        this.mockMvc.perform(post("/signIn").flashAttr("personalId", "1"))
+                .andExpect(redirectedUrl("/"))
+                .andExpect(status().isFound())
+                .andExpect(flash().attributeExists("personalId"));
+    }
+
+    @Test
+    void testRegisterUser_Successful_ShouldRedirectToIndexPage() throws Exception {
+        this.mockMvc.perform(post("/register"))
+                .andExpect(redirectedUrl("/"))
+                .andExpect(status().isFound());
+    }
+
+    @Test
+    void testRegisterUser_Unsuccessful_ShouldRedirectToRegistrationPage() throws Exception {
+        when(userService.existsByPersonalId(anyString())).thenReturn(true);
+        this.mockMvc.perform(post("/register")
+                .flashAttr("user", new UserDto("", "", "", "", "")))
+                .andExpect(redirectedUrl("user/registration"))
+                .andExpect(status().isFound());
     }
 }
